f77c0010535c8da150365bb52f8525c739c2a84f
PhucTL <lethienphuc1009@gmail.com>
2024-10-02 11:05:31 +0700

    Update About

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header/Header.jsx'
-import Home from './pages/Home/Home.jsx'
-import Login from './pages/Login/Login.jsx'
-import About from './pages/About/About.jsx'
-import Register from './pages/Register/Register.jsx'
-import CurrentAuction from './pages/CurrentAuction/CurrentAuction.jsx'
-import PastAuction from './pages/PastAuction/PastAuction.jsx'
-import DemoAxios from './components/DemoAxios.jsx';
-import Footer from './components/Footer/Footer.jsx';
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Login/Login.jsx";
+import About from "./pages/About/About.jsx";
+import Register from "./pages/Register/Register.jsx";
+import CurrentAuction from "./pages/CurrentAuction/CurrentAuction.jsx";
+import PastAuction from "./pages/PastAuction/PastAuction.jsx";
+import DemoAxios from "./components/DemoAxios.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+// import User from "./staff/User/User.jsx";
+// import Request from "./staff/Request.jsx";
+// import Auction from "./staff/Auction.jsx";
+// import Header from "./staff/Header/Header.jsx";
+// import Home from "./staff/Home.jsx";
 
 function App() {
-
   return (
     <Router>
-      <div className='wrapper'>
+      <div className="wrapper">
         <Header />
-        <div className='main'>
+        <div className="main">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
@@ -30,7 +33,25 @@ function App() {
         <Footer />
       </div>
     </Router>
-  )
+  );
 }
+// function App() {
+//   return (
+//     <Router>
+//       <div className="wrapper">
+//         <Header />
+//         <div className="main">
+//           <Routes>
+//             <Route path="/home" element={<Home />} />
+//             <Route path="/user" element={<User />} />
+//             <Route path="/request" element={<Request />} />
+//             <Route path="/auction" element={<Auction />} />
+//           </Routes>
+//         </div>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
 
-export default App
+export default App;
