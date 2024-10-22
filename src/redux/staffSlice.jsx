f879fda422fb8f08f32fa5e0e5dd70da24acd7f6
Eddyy1410 <eddynguyen14@gmail.com>
2024-10-22 14:55:20 +0700

    Modify Manage Staff Page

--- src/redux/staffSlice.jsx
+++ src/redux/staffSlice.jsx
@@ -4,48 +4,27 @@ const staffSlice = createSlice({
   name: "staff",
   initialState: {
     staffs: {
-      allStaffs: [],
-      isFetching: false,
-      error: false,
-    },
-    addstaffs: {
-      addStaffs: [],
+      allStaffs: null,
       isFetching: false,
       error: false,
     },
   },
   reducers: {
-    getStaffStart: (state) => {
-      state.staffs.isFetching = true;
-    },
-    getStaffSuccess: (state, action) => {
-      state.staffs.isFetching = false;
-      state.staffs.allStaffs = action.payload;
-    },
-    getStaffFailed: (state) => {
-      state.staffs.isFetching = false;
-      state.getStaff.error = true;
-    },
-
-    addStaffStart: (state) => {
-      state.staffs.isFetching = true;
-    },
-    addStaffSuccess: (state) => {
-      state.staffs.isFetching = false;
-      state.staffs.addStaffs.push(action.payload);
-    },
-    addStaffFailed: (state) => {
-      state.staffs.isFetching = false;
-      state.staffs.error = true;
-    },
+      getStaffsStart: (state) => {
+        state.staffs.isFetching = true
+      },
+      getStaffsSuccess: (state,action) => {
+        state.staffs.isFetching = false,
+        state.staffs.allStaffs = action.payload
+      },
+      getStaffsFailed: (state) => {
+        state.staffs.error = true
+      }
   },
 });
 export const {
-  getStaffStart,
-  getStaffSuccess,
-  getStaffFailed,
-  addStaffStart,
-  addStaffSuccess,
-  addStaffFailed,
+  getStaffsStart,
+  getStaffsSuccess,
+  getStaffsFailed
 } = staffSlice.actions;
 export default staffSlice.reducer;
